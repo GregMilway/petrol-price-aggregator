ad9ed35d72daacbc514597f9625afb413f049bf7
greg <greg.milway@gmail.com>
2026-03-09 17:13:29 +0000

    Add utility functions to convert opening times to and from DB rows

--- aggregator/utils.py
+++ aggregator/utils.py
@@ -5,6 +5,7 @@ from typing import Any
 
 import requests
 from config import FF_BASE_URL
+from schema.station import OpeningTimes
 
 END_OF_BATCH_MESSAGE = re.compile(r"Requested batch \d+ is not available")
 utils_logger = logging.getLogger("utils")
@@ -82,3 +83,50 @@ def get_stations(
         token,
         **params,
     )
+
+
+def opening_times_to_rows(opening_times: OpeningTimes, node_id: str) -> list[dict[str, Any]]:
+    rows = []
+    for day, info in opening_times.usual_days:
+        row = {
+            "weekday": day,
+            "type": None,
+            "open": info.open,
+            "close": info.close,
+            "is_24_hours": info.is_24_hours,
+            "station_id": node_id,
+        }
+        rows.append(row)
+    bh = opening_times.bank_holiday
+    rows.append(
+        {
+            "weekday": "bank_holiday",
+            "type": bh.type,
+            "open": bh.open_time,
+            "close": bh.close_time,
+            "is_24_hours": bh.is_24_hours,
+            "station_id": node_id,
+        }
+    )
+    return rows
+
+
+def rows_to_opening_times(rows: list[dict[str, Any]]) -> OpeningTimes:
+    assert len(rows) == 8
+    usual_days = {}
+    bank_holiday = {}
+    for row in rows:
+        weekday = row["weekday"]
+        if weekday == "bank_holiday":
+            bank_holiday["type"] = row["type"]
+            bank_holiday["open_time"] = row["open"]
+            bank_holiday["close_time"] = row["close"]
+            bank_holiday["is_24_hours"] = row["is_24_hours"]
+        else:
+            usual_days[weekday] = {
+                "open": row["open"],
+                "close": row["close"],
+                "is_24_hours": row["is_24_hours"],
+            }
+    data = {"usual_days": usual_days, "bank_holiday": bank_holiday}
+    return OpeningTimes(**data)
